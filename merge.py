b98b775b72598a3073d97132705f49fbb3efafb9
NeilQue <9que36bryant@gmail.com>
2023-07-12 10:37:04 +0800

    removed redundancy

--- merge.py
+++ merge.py
@@ -3,9 +3,8 @@ import pandas as pd
 waterlevel_cols = ['datetime', 'water_level']
 rainfall_cols = ['datetime', 'station', '1hr', '3hr', '6hr', '12hr', '24hr']
 
-waterlevel_df = pd.read_csv('wl_data.csv', header=None, names=waterlevel_cols)
-rainfall_df = pd.read_csv('rf_data.csv', header=None, names=rainfall_cols)
+waterlevel_df = pd.read_csv('wl_data.csv', header=None, names=waterlevel_cols, engine='pyarrow')
+rainfall_df = pd.read_csv('rf_data_new.csv', header=None, names=rainfall_cols, engine='pyarrow')
 
 final_df = pd.merge(waterlevel_df, rainfall_df, on='datetime', how='left')
 final_df.to_csv('data', index=False)
-final_df.to_excel('data.xlsx', sheet_name='Sheet1')
